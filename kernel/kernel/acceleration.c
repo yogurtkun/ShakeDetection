89b2ea50793df0a2f30a6516e46d79d9cbaa7404
Cauchyky <ky2372@columbia.edu>
2017-10-13 21:14:37 -0400

    fix compile bug

--- kernel/kernel/acceleration.c
+++ kernel/kernel/acceleration.c
@@ -6,14 +6,15 @@
  * and writes it to the kernel
  */
 
-#include <linux/syscall.h>
+#include <linux/syscalls.h>
 #include <linux/acceleration.h>
 #include <linux/cred.h>
-#include <asm-generic/uaccess.h>
+#include <asm/uaccess.h> // copy_from_user
 #include <linux/string.h>
+#include <linux/rwlock.h>
 
 static struct dev_acceleration acc;
-DEFINE_RWLOCK(mr_rwlock);
+DEFINE_RWLOCK(lock);
 
 /**
  * sys_set_acceleration - set the current device acceleration
@@ -28,15 +29,15 @@ SYSCALL_DEFINE1(set_acceleration, struct dev_acceleration __user *, acceleration
 	struct dev_acceleration *tmp;
 
 	if (current_uid()!=0) {
-		return -EACCESS;
+		return -EACCES;
 	}
 
 	if(copy_from_user(tmp, acceleration, sizeof(struct dev_acceleration)))
 		return -EFAULT;
 
-	write_lock(&mr_rwlock);
+	write_lock(&lock);
 	memcpy(&acc, tmp, sizeof(struct dev_acceleration));
-	write_unlock(&mr_lock);
+	write_unlock(&lock);
 
 	return 0;
 }
