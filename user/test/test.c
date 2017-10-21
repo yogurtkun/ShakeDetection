d3efcb8edc9a99ad3c78f0403d7791e67b4a277b
Kaho Chan <kaelchan@hotmail.com>
2017-10-20 23:53:56 -0400

    a untested prototype of test program (problem 4)

--- user/test/test.c
+++ user/test/test.c
@@ -9,18 +9,62 @@
 
 #include "test.h"
 
+int detect(int eid, int id) {
+	pid_t pid;
+	pid = fork();
+	if (pid < 0){ 
+		printf("Fork failed\n");
+		exit(EXIT_FAILURE);
+	}
+	if (pid > 0)
+		return 0;
+	/* children */
+	int ret;
+	if (ret = syscall(__NR_accevt_wait, &eid)) {
+		/* handling the failure */
+	}
+	if (id == 0)
+		printf("%d detected a horizontal shake", id);
+	else if (id == 1)
+		printf("%d detected a vertical shake", id);
+	else if (id == 2)
+		printf("%d detected a shake", id);
+	return ret;
+}
+
 int main(int argc, char const *argv[])
 {
-	struct acc_motion xmotion;
-	xmotion.dlt_x = 1000;
-	xmotion.dlt_y = 0;
-	xmotion.dlt_z = 0;
-	xmotion.frq = 10;
+	/* TODO: decide the value */
+	struct acc_motion xshake;
+	xshake.dlt_x = 1000;
+	xshake.dlt_y = 0;
+	xshake.dlt_z = 0;
+	xshake.frq = 10;
 
-	printf("Hello world from user\n");
-	int eid;
-	eid = syscall(__NR_accevt_create,&xmotion);
-	syscall(__NR_accevt_wait, eid);
-	printf("X motion!\n");
+	struct acc_motion yshake;
+	yshake.dlt_x = 0;
+	yshake.dlt_y = 1000;
+	yshake.dlt_z = 0;
+	yshake.frq = 10;
+
+	struct acc_motion shake;
+	shake.dlt_x = 1000;
+	shake.dlt_y = 1000;
+	shake.dlt_z = 0;
+	shake.frq = 10;
+
+	int xeid, yeid, eid;
+	xeid = syscall(__NR_accevt_create, &xshake);
+	yeid = syscall(__NR_accevt_create, &yshake);
+	eid  = syscall(__NR_accevt_create, &shake);
+	/* TODO: discuss the meaning of process identifier */
+	detect(xeid, 0);
+	detect(yeid, 1);
+	detect(eid, 2);
+	/* close all opened events after a peiod of time */
+	sleep(61);
+	syscall(__NR_accevt_destroy, &xeid);
+	syscall(__NR_accevt_destroy, &yeid);
+	syscall(__NR_accevt_destroy, &eid);
 	return 0;
 }
