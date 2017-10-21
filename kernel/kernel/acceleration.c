a0402986806976c80939ba6bed06c5a30edb29fd
Kaho Chan <kaelchan@hotmail.com>
2017-10-21 10:35:07 -0400

    remove the check for ref_count in destroy()

--- kernel/kernel/acceleration.c
+++ kernel/kernel/acceleration.c
@@ -345,12 +345,8 @@ SYSCALL_DEFINE1(accevt_destroy, int , event_id){
 
 	printk(KERN_INFO "step 2");	
 
-	int ref_times = atomic_read(&to_destroy_event->ref_count);
-
-	if (ref_times > 0) {
-		to_destroy_event->triggered = 1;
-		wake_up(&to_destroy_event->wait_queue);
-	}
+	to_destroy_event->triggered = 1;
+	wake_up(&to_destroy_event->wait_queue);
 
 	printk(KERN_INFO "step 3");
 
